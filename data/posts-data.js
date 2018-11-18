8fd18a5a54155ef96cddeefc5d0bc5fffb2ff809
songyouyu <songyy950208@163.com>
2018-11-18 15:17:11 +0800

    小程序的模板化与模块化

---
+++ data/posts-data.js
@@ -0,0 +1,42 @@
+var local_database = [
+	{
+		date: "Sep 18 2018",
+		title: "今天也是充满希望的一天",
+		imgSrc: "/images/post/1.jpg",
+		avatar: "/images/avatar/2.jpg",
+		content: "当凛冬安下她的灵魂，散下她的梦魇后，遗留在昨日繁华里的所有爱恨情仇，将会被一一埋葬。				  碧落黄泉，阴阳两隔，不再纠结，等待下一场轮回的上映，再写一段没有结局的故事;再话一					  场遍体鳞伤的桑麻.",
+		reading: "112",
+		collection: "96",
+	},
+	{
+		date: "Nov 20 2016",
+		title: "比利·林恩的中场故事",
+		imgSrc: "/images/post/2.jpg",
+		avatar: "/images/avatar/3.jpg",
+		content: "一 “李安是一位绝不会重复自己的导演，本片将极富原创性李安众所瞩目的新片《比利林恩漫长的中场休息》，正式更名《半场无战事》。",
+		reading: "98",
+		collection: "124",
+	},
+	{
+		date: "Nov 12 2016",
+		title: "从视觉到触觉 这款VR手套能给你真实触感",
+		content: "8月29日消息，据国外媒体VentureBeat报道，一家名为Dexta Robotics的公司最近发布了一款有望变革虚拟现实手部追踪与交互方式的新产品",
+		imgSrc: "/images/post/3.jpg",
+		avatar: "/images/avatar/2.png",
+		reading: "29",
+		collection: "83",
+	},
+	{
+		date: "Nov 20 2016",
+		title: "当我们在谈论经济学时，我们在谈论什么?",
+		content: "引言在我跟学生课后交流时，以及我在知乎上阅读有关“经济”问题的论题时，经常会遇到这样的情况：...",
+		imgSrc: "/images/post/2.jpg",
+		avatar: "/images/avatar/1.png",
+		reading: "62",
+		collection: "92",
+	},
+]
+
+module.exports = {
+	postList: local_database
+}
